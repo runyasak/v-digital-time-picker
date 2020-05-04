72bf86bf963e81b88f7e614509eac5923fc6c29c
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-05-05 00:29:09 +0700

    Add vue/html-self-closing eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,16 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/html-self-closing': ['error', {
+      "html": {
+        "void": "never",
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }]
   },
   overrides: [
     {
