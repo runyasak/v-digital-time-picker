720315d0e4dca0ee02729ee8550f2a5acf1d40a8
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-05-05 02:06:35 +0700

    Add import/order rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/order': ['error', {'newlines-between': 'always'}],
     'vue/html-self-closing': ['error', {
       "html": {
         "void": "never",
