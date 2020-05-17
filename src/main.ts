5768c15ef8b788456575de3123e920c11d146a9a
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-05-17 15:06:04 +0700

    Update vee-validate rules and components to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import { ValidationProvider, extend } from 'vee-validate'
+import { required } from 'vee-validate/dist/rules'
 
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+
+extend('required', required)
+
+Vue.component('ValidationProvider', ValidationProvider)
 
 new Vue({
   vuetify,
